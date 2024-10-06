3185e3b9ca18aa68b7ba37dbf506b73bf54f717f
1chima6 <chijiokechimarokem@gmail.com>
2024-10-05 22:17:30 -0500

    bugfix - fix colours on page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,17 +23,13 @@ export default async function Index() {
       <h1 className="mb-8 text-2xl font-bold">Navigation</h1>
       <div className="flex gap-4">
         <Link href="/cbh">
-          <button className="rounded bg-blue-500 px-4 py-2 text-white">
-            CBH
-          </button>
+          <button className="rounded bg-white px-4 py-2 text-black">CBH</button>
         </Link>
         <Link href="/hr">
-          <button className="rounded bg-green-500 px-4 py-2 text-white">
-            HR
-          </button>
+          <button className="rounded bg-white  px-4 py-2 text-black">HR</button>
         </Link>
         <Link href="/emp">
-          <button className="rounded bg-red-500 px-4 py-2 text-white">
+          <button className="rounded bg-white px-4 py-2 text-black">
             Employees
           </button>
         </Link>
@@ -41,7 +37,7 @@ export default async function Index() {
 
       {/* Optionally, show Supabase connection status */}
       {isSupabaseConnected ? (
-        <p className="mt-8 text-green-600">Supabase is connected!</p>
+        <p className="mt-8 text-white">Supabase is connected!</p>
       ) : (
         <p className="mt-8 text-red-600">Supabase is not connected.</p>
       )}
